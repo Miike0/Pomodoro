2b1211756a05221afab83378ebd4566d232dac42
Miikeo <Miguel.A.INCO@gmail.com>
2022-10-22 01:00:54 -0500

    Functions to the timer general, to break timer, start task and start breah, and functions to finish the task

--- JS/main.js
+++ JS/main.js
@@ -38,4 +38,82 @@ function renderTask() {
         `;
     });
 
+    const taskContainer = document.querySelector('#tasks');
+    taskContainer.innerHTML = html.join('');
+
+    const startButtons = document.querySelectorAll('.task .start-button');
+    startButtons.forEach(button => {
+        button.addEventListener('click', e => {
+            if (!timer) {
+                const id = button.getAttribute('data-id');
+                startButtonHandler(id);
+                button.textContent = 'In progress...';
+            }
+        });
+    });
+}
+
+function startButtonHandler (id) {
+    time = 25 * 60;
+    current = id;
+    const taskIndex = tasks.findIndex(task => task.id == id);
+    
+    taskName.textContent = tasks[taskIndex].tittle;
+    renderTime()
+
+    timer = setInterval(() => {
+        timehandler(id);
+    }, 1000);
+}
+
+function timehandler (id) {
+    time--;
+    renderTime();
+
+    if (time === 0) {
+        clearInterval(timer);
+        markCompleted(id);
+        timer = null;
+        renderTask();
+        startBreak();
+    }
+}
+
+function startBreak () {
+    time = 5 * 60;
+    taskName.textContent = 'Break';
+    renderTime();
+    timerBreak = setInterval(() => {
+        timerBreakHandler();
+    }, 1000);
+}
+
+function timerBreakHandler () {
+    time--;
+    renderTime();
+
+    if (time === 0) {
+        clearInterval(timerBreak);
+        current = null;
+        timerBreak = null;
+        taskName.textContent = '';
+        renderTask();
+        
+    }
+    
+}
+
+function renderTime () {
+    const timeDiv = document.querySelector('#time #value');
+    const minutes = parseInt(time / 60);
+    const seconds = parseInt(time % 60);
+
+    timeDiv.textContent = `${minutes < 10 ? '0' : ''}${minutes}:${seconds < 10 ? '0' : ''}${seconds}`;
+
+
+}
+
+function markCompleted (id) {
+    const taskIndex = tasks.findIndex ((task) => task.id == id);
+    tasks[taskIndex].completed = true;
 }

1a1f0b16fc801c90cd64428348d1beb6c7ec2e48
Miikeo <Miguel.A.INCO@gmail.com>
2022-10-22 00:56:49 -0500

    Variables and query selector requerid declared

---
+++ JS/main.js
@@ -0,0 +1,10 @@
+const tasks = [];
+let time = 0; 
+let timer = null;
+let timerBreak = null;
+let current = null;
+
+const bAdd = document.querySelector('#bAdd');
+const idTask = document.querySelector('#idTask');
+const form = document.querySelector('#form');
+const taskName = document.querySelector('#time #taskName');
